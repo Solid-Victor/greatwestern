569fd5b8e09a2bbfb5f861c49e8d1535ac4a6378
Solid-Victor <victorcontents@gmail.com>
2025-01-27 12:14:16 +0100

    added large files

--- next.config.js
+++ next.config.js
@@ -1,26 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(mp4|webm)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/media/',
-          outputPath: 'static/media/',
-          name: '[name].[hash].[ext]',
-        },
-      },
-    });
-    return config;
-  },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
-    domains: ['localhost'], // Add your production domain
-    unoptimized: process.env.NODE_ENV === 'production' ? false : true,
+    unoptimized: true, // Required for static exports
   },
-  // Enable if using static export
-  output: 'export' 
+  output: 'export'
 };
 
 module.exports = nextConfig;
